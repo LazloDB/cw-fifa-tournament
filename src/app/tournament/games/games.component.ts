5d064bb66566480f4932461f7e641d21ec410e38
LazloDB <lazlodebock@hotmail.com>
2018-10-02 12:32:18 +0200

    sort poule matches

--- src/app/tournament/games/games.component.ts
+++ src/app/tournament/games/games.component.ts
@@ -29,14 +29,26 @@ export class GamesComponent implements OnInit {
   }
 
   createMatches(poule) {
-    const result = [];
+    let result = [];
     for (let i = 0; i < poule.length; i++) {
       for (let j = i + 1; j < poule.length; j++) {
         result.push([{name: poule[i].name, club: this.clubService.getClub()}, {name: poule[j].name, club: this.clubService.getClub()}]);
       }
     }
 
+    result = this.sortPoule(result, poule.length);
+
     return result;
+  }
+
+  sortPoule(result, length): Array<any> {
+    if (length === 4) {
+      return [result[0], result[5], result[1], result[4], result[2], result[3]];
+    } else if (length === 5) {
+      return [result[0], result[7], result[3], result[4], result[9], result[1], result[5], result[8], result[2], result[6]];
+    } else {
+      return result;
+    }
   }
 
 }
