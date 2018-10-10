df559eb6c798df4bdb0e845c95c3d3a7740e32e8
LazloDB <lazlodebock@hotmail.com>
2018-10-10 10:42:32 +0200

    setup poules to knockouts

--- src/app/tournament/poules/poules.component.ts
+++ src/app/tournament/poules/poules.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-poules',
@@ -7,10 +7,11 @@ import { Component, Input } from '@angular/core';
 })
 export class PoulesComponent {
   @Input() poules: Array<any>;
+  @Output() finish = new EventEmitter();
 
   constructor() { }
 
   handleGames(e) {
-    console.log('ready');
+    this.finish.emit();
   }
 }
